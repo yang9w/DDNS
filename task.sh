e8fcf5e0ae8f44859b150e29162bcde352c183f6
yang9w <yang9w@gmail.com>
2020-03-05 19:36:42 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -5,3 +5,4 @@ CMD="\"$RUN_DIR/run.py\" -c \"$RUN_DIR/config.json\" >> \"$RUN_DIR/run.log\""
 
 echo "*/5 * * * *   root    $CMD" > /etc/cron.d/ddns;
 /etc/init.d/cron reload;
+#如果centos 8 报错第七行使用此命令替换第七行：service crond reload;
